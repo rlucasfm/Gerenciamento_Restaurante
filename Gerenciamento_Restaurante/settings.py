7b54688330b2b434ce0651e64e4363c83203e8e6
Richard Lucas <richardlucasfm@gmail.com>
2019-06-07 17:10:32 -0300

    Update settings.py

--- Gerenciamento_Restaurante/settings.py
+++ Gerenciamento_Restaurante/settings.py
@@ -77,22 +77,22 @@ WSGI_APPLICATION = 'Gerenciamento_Restaurante.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'controle_pedidos',
-        'USER': 'django',
-        'PASSWORD': 'django',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'controle_pedidos',
+#        'USER': 'django',
+#        'PASSWORD': 'django',
+#        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+#        'PORT': '3306',
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
